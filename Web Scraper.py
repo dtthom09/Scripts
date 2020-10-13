66d4365650c6135d73aeb7c02276e7928e13724d
Dylan T Thomas <dtthom09@cardmail.louisville.edu>
2020-10-13 13:35:37 -0400

    grabbing data from newegg

---
+++ Web Scraper.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen as Ureq
+from bs4 import BeautifulSoup as Soup
+
+# website to scrape data from
+my_url = 'https://www.newegg.com/p/pl?d=video+cards'
+
+# opening up connection, grabbing the page
+uClient = Ureq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+# html parsing
+page_soup = Soup(page_html, 'html.parser')
+
+# testing to see that page_soup is getting html from url
+page_soup.h1
+page_soup.p
+
+# grabs each product
+containers = page_soup.findAll("div", {"class": "item-container"})
