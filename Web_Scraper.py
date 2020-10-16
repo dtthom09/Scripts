e605c74f2a034f66cd10e42e21a62f7bbc3473e9
Dylan T Thomas <dtthom09@cardmail.louisville.edu>
2020-10-16 18:13:50 -0400

    added code for csv

--- Web_Scraper.py
+++ Web_Scraper.py
@@ -20,8 +20,28 @@ page_soup.p
 containers = page_soup.findAll("div", {"class": "item-container"})
 len(containers)
 
+# creating csv file
+filename = "products.csv"
+f = open(filename, "w")
+
+# call headers
+headers = "brand, product_name, shipping\n"
+
+# writes headers then starts a new line
+f.write(headers)
+
 for container in containers:
-    title = container.div.div.a["title"]
+    title = container.div.a.img["title"]
     title_container = container.findAll("a", {"class":"item-title"})
     product_name = title_container[0].text
-    
+    shipping_container = container.findAll("li", {"class":"price-ship"})
+    shipping = shipping_container[0].text.strip()
+
+    print("title: " + title)
+    print("product name: " + product_name)
+    print("shipping: " + shipping)
+
+    product_name.replace(",", ";")
+    f.write(brand + "," + product_name + "," + shipping + "\n")
+
+f.close()
